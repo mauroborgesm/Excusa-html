a0eb80ab0f0bc2cd3a65f4e4f465ec619f06de55
Mauro Borges <mauroborgesm@gmail.com>
2023-03-07 01:54:27 +0000

    Segunda version sin boton

--- Excuse Generator/app.js
+++ Excuse Generator/app.js
@@ -1,5 +1,25 @@
-let who = ['the dog','my granma','his turtle','my bird'];
+let who = ['The dog','My grandma','His turtle','My bird'];
+let action = ['ate','peed','crushed','broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];  
 
-let what = ['eat','pissed','crushed','broked'];
 
-let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+function ArrayPicker(item)
+{
+    let concatenated = item[Math.floor(Math.random()*item.length)];
+    return concatenated;
+}
+function ArrayConcatenator(who, action, what, when)
+{
+    let sentence = who+" "+action+" "+what+" "+when;
+    return sentence;
+}
+
+const concatenatedSentence = ArrayConcatenator(ArrayPicker(who), ArrayPicker(action), ArrayPicker(what), ArrayPicker(when));
+
+function WriteToHtml()
+{
+    console.log(concatenatedSentence);
+    document.getElementById("excuse").innerHTML = concatenatedSentence;
+}
+document.addEventListener("DOMContentLoaded", WriteToHtml);
