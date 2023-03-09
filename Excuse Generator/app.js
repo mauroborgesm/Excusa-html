61d9f90db0682b945149e1f474310065bb13fad1
Mauro Borges <mauroborgesm@gmail.com>
2023-03-09 01:51:19 +0000

    Version de codigo limpio

--- Excuse Generator/app.js
+++ Excuse Generator/app.js
@@ -4,22 +4,21 @@ let what = ['my homework', 'the keys', 'the car'];
 let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];  
 
 
-function ArrayPicker(item)
-{
+function ArrayPicker(item){
     let concatenated = item[Math.floor(Math.random()*item.length)];
     return concatenated;
 }
-function ArrayConcatenator(who, action, what, when)
-{
+
+function ArrayConcatenator(who, action, what, when){
     let sentence = who+" "+action+" "+what+" "+when;
     return sentence;
 }
 
 const concatenatedSentence = ArrayConcatenator(ArrayPicker(who), ArrayPicker(action), ArrayPicker(what), ArrayPicker(when));
 
-function WriteToHtml()
-{
+function WriteToHtml(){
     console.log(concatenatedSentence);
     document.getElementById("excuse").innerHTML = concatenatedSentence;
 }
+
 document.addEventListener("DOMContentLoaded", WriteToHtml);
